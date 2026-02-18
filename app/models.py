91c6767eb0a44a48e39b1cdfb5d65207b9bb6798
Preeti Chavan <preeti.p.c.tas@gmail.com>
2026-02-18 11:21:45 +0530

    Added Docker setup and updated FastAPI project

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
-    is_active = Column(Boolean, default=False) 
+    is_active = Column(Boolean, default=True) 
 
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=True)
